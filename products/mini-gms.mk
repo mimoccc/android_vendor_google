4a838ac2246c557aea7450dd4107e90b3dc34cb5
android <trevd1234@gmail.com>
2014-03-02 10:35:55 +0000

    Add priv-app directory and playstore only product

---
+++ products/mini-gms.mk
@@ -0,0 +1,12 @@
+# Add Google Apps and Services
+PRODUCT_PACKAGES += \
+	GoogleLoginService.apk \
+	GoogleServicesFramework.apk \
+	Phonesky.apk \
+	PrebuiltGmsCore.apk 
+
+# Overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+         ro.setupwizard.mode=DISABLED \
+
+
